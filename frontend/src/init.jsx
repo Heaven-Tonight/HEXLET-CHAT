0ed9c8ddc7ff474e348475a519888dbe577456c8
Svetlna <svetlana@dev.com>
2023-12-14 12:24:06 +0300

    added Rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,9 +1,10 @@
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import { BrowserRouter } from 'react-router-dom';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import App from './components/App';
 import { Provider } from 'react-redux';
-import { AuthProvider, ModalProvider } from './providers/index.jsx';
+import { AuthProvider, ModalProvider, DropdownProvider } from './providers/index.jsx';
 import resources from './locales/index.js';
 import store from './store/index.js';
 
@@ -16,20 +17,37 @@ const init = async () => {
       resources,
       fallbackLng: 'ru',
     });
+  
+  const rollbarConfig = {
+    accessToken: '7737b8544bde41b8bf87386aaf3e3e87',
+    environment: 'testenv',
+  };
+  
+  function TestError() {
+    const a = null;
+    return a.hello();
+  }
 
   return (
-    <AuthProvider>
-      <I18nextProvider i18n={i18n}>
-        <ModalProvider>
-          <BrowserRouter>
-            <Provider store={store}>
-              <App />
-            </Provider>
-          </BrowserRouter>
-        </ModalProvider>
-      </I18nextProvider>
-    </AuthProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <TestError />
+      </ErrorBoundary>
+      <AuthProvider>
+        <I18nextProvider i18n={i18n}>
+          <ModalProvider>
+            <BrowserRouter>
+              <Provider store={store}>
+                <App />
+              </Provider>
+            </BrowserRouter>
+          </ModalProvider>
+        </I18nextProvider>
+      </AuthProvider>
+    </RollbarProvider>
+    
   );
 };
 
 export default init;
+
